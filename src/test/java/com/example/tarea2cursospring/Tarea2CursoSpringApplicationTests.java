34c2d0c9b7e2bf3f769df1ceb96d78e999ff695e
Sergio <sergio.ume@hotmail.com>
2024-04-18 16:36:50 +0100

    uso de autowired en test

--- src/test/java/com/example/tarea2cursospring/Tarea2CursoSpringApplicationTests.java
+++ src/test/java/com/example/tarea2cursospring/Tarea2CursoSpringApplicationTests.java
@@ -1,10 +1,29 @@
 package com.example.tarea2cursospring;
 
+import com.example.tarea2cursospring.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Tarea2CursoSpringApplicationTests {
+
+	@Autowired//inyeccion del contexto
+	ApplicationContext applicationContext;
+
+	@Autowired//inyeccion del controlador
+	MyController myController;
+	@Test
+	void testGetControllerFromCtx() {
+		MyController controller =  applicationContext.getBean(MyController.class);
+		System.out.println(controller.diHola());
+	}
+
+	@Test
+	void testAotowireOfController() {
+		System.out.println(myController.diHola());
+	}
 
 	@Test
 	void contextLoads() {
